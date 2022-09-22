73cfe654e7749ae9851c5a7afa36bf70dc1d6c44
Pavel <vasilyev.x@mail.com>
2022-09-22 11:28:00 +0300

    Initial commit

---
+++ src/main/java/pro/sky/cousrse2/employeelist/EmployeeListApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cousrse2.employeelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/cousrse2/employeelist/EmployeeListApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cousrse2.employeelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
